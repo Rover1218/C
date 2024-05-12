9755c706318b40d66c4f4a70957c76cb8bc44729
Rover <86872566+Rover1218@users.noreply.github.com>
2024-05-12 08:39:08 +0530

    first commit

---
+++ Assignement 3/leapyear.c
@@ -0,0 +1,32 @@
+/*#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter year:");
+    scanf("%d", &a);
+    if (a % 400 == 0)
+        printf("It is a leap year\n");
+    else if (a % 100 == 0)
+        printf("It is not a leap year\n");
+    else if (a % 4 == 0)
+        printf("It is a leap year");
+    else
+        printf("It is not a leap year");
+    return 0;
+}*/
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter year: ");
+    scanf("%d", &a);
+    if (a % 4 == 0 && (a % 100 != 0 || a % 400 == 0))
+    {
+        printf("It is a leap year");
+    }
+    else
+    {
+        printf("It is not a leap year");
+    }
+    return 0;
+}
